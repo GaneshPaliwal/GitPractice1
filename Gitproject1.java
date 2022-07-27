5ee298ce07524184125108fabfc74724f687b6dd
GaneshPaliwal <ganyesh.88@gmail.com>
2022-07-27 12:43:29 +0530

    first commit1

---
+++ Gitproject1.java
@@ -0,0 +1,12 @@
+package gitproject;
+
+public class Gitproject1 {
+
+	public static void main(String[] args) {
+		
+			System.out.println("India1");
+			System.out.println("Maharashtra1");
+
+	}
+
+}
